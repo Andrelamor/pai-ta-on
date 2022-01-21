9a43229da4f77f6f10b5b73d40914afd72c0c2bf
André Amorim <52294411+Andrelamor@users.noreply.github.com>
2022-01-21 11:07:46 -0300

    add scratches for python practice

---
+++ app.py
@@ -0,0 +1,46 @@
+# intro
+print('Hello, is there anybody in there?')
+print('o----')
+print(' ||||')
+print('*' * 10)
+
+# variables
+# https://www.youtube.com/watch?v=_uQrJ0TkZlc&t=783s
+price = 10
+rating = 4.9
+name = 'Mosh'
+is_published = True
+print (price)
+
+name = "John Smith"
+age = 20
+new_patient = True
+
+Simone = "André"""
+print(Simone)
+name[1]
+print(name[1])
+print(name[-2])
+print(name[1:-1])
+print(len(name))
+print(name.title())
+print(name.strip())
+print(name.find("Smith"))
+name = name.replace("j", "k")
+print(name)
+print(name.find("John"))
+
+# Receiving input
+# https://www.youtube.com/watch?v=_uQrJ0TkZlc&t=1101s
+
+your_name = input('What is your name? ')
+print('Hi ' + your_name)
+your_color = input('What is your favourite color? ')
+print(your_name + ' likes ' + your_color)
+
+
+# Python Cheat Sheet
+# https://www.youtube.com/watch?v=_uQrJ0TkZlc&t=1336s
+
+# Type conversion
+# https://www.youtube.com/watch?v=_uQrJ0TkZlc&t=1366s
