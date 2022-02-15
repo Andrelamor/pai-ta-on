9b9894a3d7394b20b55fd685a1904c9fc3842b38
andrelamor <amorim.andrel@gmail.com>
2022-02-15 11:46:12 -0300

    add string portuguese version

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ livemark build 002_warm_up/001_terminal_101/README.md --target 002_warm_up/001_t
 livemark build 002_warm_up/002_version_control_101/README.md --target 002_warm_up/002_version_control_101/README.html --config 000_livemark/livemark.yaml && \
 livemark build 003_python_essentials/001_variables/README.md --target 003_python_essentials/001_variables/README.html --config 000_livemark/livemark.yaml && \
 livemark build 003_python_essentials/002_strings/README.md --target 003_python_essentials/002_strings/README.html --config 000_livemark/livemark.yaml && \
+livemark build 003_python_essentials/002_strings/LEIAME.md --target 003_python_essentials/002_strings/LEIAME.html --config 000_livemark/livemark.yaml && \
 
 # Start server to test builded pages
 livemark start 000_livemark/index.md --target ./index.html --config 000_livemark/livemark.yaml
